f8d7d1d3f5691acf5d39d36b00c1554d9536f049
Bella <fbfw29@icloud.com>
2023-08-11 10:16:18 -0400

    Initial Commit

---
+++ Navigation Example/Navigation_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_ExampleApp.swift
+//  Navigation Example
+//
+//  Created by ken tarsitano on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
